f11edfc05885972cd0aa8c57c82fa61a2f1077d0
Cadu M. <durval.carneiro.morais@gmail.com>
2026-09-11 10:21:07 -0300

    Feat: Adiciona listagem de alunos.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-alunos = []
 def cadastrar():
-    nome = input("Nome do aluno: ").strip()
-    if nome == "":
-        print("O nome não pode ficar vazio.")
-        return
-    for aluno in alunos:
-        if aluno.lower() == nome.lower():
-            print("Aluno já cadastrado.")
-            return
-    alunos.append(nome)
-    print("Aluno cadastrado.")
+	nome = input("Nome do aluno: ").strip()
+	if nome == "":
+		print("O nome não pode ficar vazio.")
+		return
+	for aluno in alunos:
+		if aluno.lower() == nome.lower():
+			print("Aluno já cadastrado.")
+			return
+	alunos.append(nome)
+	print("Aluno cadastrado.")
+def listar():
+	for aluno in alunos:
+		print(aluno)
 while True:
-    print("\n--- CADASTRO DE ALUNOS ---")
-    print("1 - Cadastrar")
-    print("0 - Sair")
-    opcao = input("Escolha: ")
-    if opcao == "0":
-        print("Programa encerrado.")
-        break
-    elif opcao == "1":
-        cadastrar()
-    else:
-        print("Opção inválida.")
+	print("\n--- CADASTRO DE ALUNOS ---")
+	print("1 - Cadastrar")
+	print("2 - Listar")
+	print("0 - Sair")
+	opcao = input("Escolha: ")
+	if opcao == "0":
+		print("Programa encerrado.")
+		break
+	elif opcao == "1":
+		cadastrar()
+	elif opcao == "2":
+		listar()
+	else:
+		print("Opção inválida.")
