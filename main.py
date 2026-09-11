c9a584dbdd71b7d036d5ecf68f71e246b8747b1a
Cadu M. <durval.carneiro.morais@gmail.com>
2026-09-11 10:02:01 -0300

    Feat: Adiciona cadastro de alunos

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 alunos = []
-
+def cadastrar():
+    nome = input("Nome do aluno: ")
+    alunos.append(nome)
+    print("Aluno cadastrado.")
 while True:
     print("\n--- CADASTRO DE ALUNOS ---")
+    print("1 - Cadastrar")
     print("0 - Sair")
     opcao = input("Escolha: ")
-
     if opcao == "0":
         print("Programa encerrado.")
         break
+    elif opcao == "1":
+        cadastrar()
     else:
         print("Opção inválida.")
