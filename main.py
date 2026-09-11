f791012c2d91c57767f8d9e8f58cacf78e49dd22
Cadu M. <durval.carneiro.morais@gmail.com>
2026-09-11 09:49:48 -0300

    Feat: Cria menu inicial do cadastro.

---
+++ main.py
@@ -0,0 +1,12 @@
+alunos = []
+
+while True:
+    print("\n--- CADASTRO DE ALUNOS ---")
+    print("0 - Sair")
+    opcao = input("Escolha: ")
+
+    if opcao == "0":
+        print("Programa encerrado.")
+        break
+    else:
+        print("Opção inválida.")
